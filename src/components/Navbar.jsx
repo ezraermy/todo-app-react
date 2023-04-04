54033a1ccee9d95db0f0c069b3bfbce0757aaa19
Ezra <ezraermy@gmail.com>
2023-04-04 20:11:46 +0300

    Create todo app using react library

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom"; 
+
+const links = [
+  { path: '/', text: 'Home'},
+  { path: 'about', text: 'About'},
+  { path: 'login', text: 'About'},
+  { path: 'profile', text: 'About'},
+]
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        {links.map((link) => {
+          return (
+            <li key={link.text}>
+              <NavLink to={link.path}>{link.text}</NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+  
+}
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import TodoApp from '@/components/TodoApp';
+import Layout from "@/components/Layout";
+import '@/styles/app.css'
+
+const domContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(domContainer);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Layout />
+      <TodoApp />
+    </Router>
+  </React.StrictMode>
+);
